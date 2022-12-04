6339bb3d3cd80a3afff72caa5e570cf17c975749
HyunggiPark <phg9587@naver.com>
2022-12-04 20:15:40 +0900

    feat(mm.c): mm_init 내용 추가 / 최초 가용 블록으로 힙 생성하기

--- mm.c
+++ mm.c
@@ -70,10 +70,26 @@ team_t team = {
 #define NEXT_BLKP(bp) ((char*)(bp) + GET_SIZE(((char*)(bp) - WSIZE)))
 #define PREV_BLKP(bp) ((char*)(bp) - GET_SIZE(((char*)(bp) - DSIZE)))
 
+void* heap_listp;
+
 /*
- * mm_init - initialize the malloc package.
+ * mm_init - initialize the malloc package. , 최초 가용 블록으로 힙 생성하기
  */
 int mm_init(void) {
+    /* Create the initial empty heap */
+    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void*) -1)
+        return -1;
+
+    PUT(heap_listp, 0); // Allignment padding
+    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1)); // Prologue header
+    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1)); // Prologue footer
+    PUT(heap_listp + (3 * WSIZE), PACK(0, 1)); // Epilogue header
+    heap_listp += (2 * WSIZE);
+
+    /* Extend the empty heap with a free block of CHUNKSIZE bytes */
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
+        return -1;
+
     return 0;
 }
 
