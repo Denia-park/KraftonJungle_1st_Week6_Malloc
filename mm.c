e18a1c7e703122db65e88b435b054fd87242b68b
HyunggiPark <phg9587@naver.com>
2022-12-04 22:17:49 +0900

    feat(mm.c): mm_free, coalesce 블록을 반환하고 경계 태그 연결을 사용해서 상수 시간에 인접 가용 블록들과 통합한다.

--- mm.c
+++ mm.c
@@ -73,6 +73,7 @@ team_t team = {
 void* heap_listp;
 
 static void* extend_heap(size_t size);
+static void* coalesce(void* bp);
 
 /*
  * mm_init - initialize the malloc package. , 최초 가용 블록으로 힙 생성하기
@@ -113,7 +114,12 @@ void* mm_malloc(size_t size) {
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void* ptr) {
+void mm_free(void* bp) {
+    size_t size = GET_SIZE(HDRP(bp));
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    coalesce(bp);
 }
 
 /*
@@ -153,15 +159,32 @@ static void* extend_heap(size_t words) {
     return coalesce(bp);
 }
 
+static void* coalesce(void* bp) {
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
 
+    if (prev_alloc && next_alloc) {
+        // Case 1
+        return bp;
+    } else if (prev_alloc && !next_alloc) {
+        // Case 2
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    } else if (!prev_alloc && next_alloc) {
+        // Case 3
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    } else {
+        // Case 4
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
 
-
-
-
-
-
-
-
-
-
-
+    return bp;
+}
