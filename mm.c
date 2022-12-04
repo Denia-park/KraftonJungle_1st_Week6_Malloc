9ba49746c193e1b5803eadeabbed2d6dd274ceeb
HyunggiPark <phg9587@naver.com>
2022-12-04 20:50:26 +0900

    feat(mm.c): extend_heap 새 가용 블록으로 힙 확장하기

--- mm.c
+++ mm.c
@@ -72,6 +72,8 @@ team_t team = {
 
 void* heap_listp;
 
+static void* extend_heap(size_t size);
+
 /*
  * mm_init - initialize the malloc package. , 최초 가용 블록으로 힙 생성하기
  */
@@ -133,6 +135,23 @@ void* mm_realloc(void* ptr, size_t size) {
     return newptr;
 }
 
+static void* extend_heap(size_t words) {
+    char* bp;
+    size_t size;
+
+    /* Allocate an even number of words to maintain alignment */
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if ((long) (bp = mem_sbrk(size)) == -1)
+        return NULL;
+
+    /* Initialize free block header/footer and the epilogue header */
+    PUT(HDRP(bp), PACK(size, 0)); // Free block header
+    PUT(FTRP(bp), PACK(size, 0)); // Free block footer
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); // New epilogue header
+
+    /* Coalesce if the previous block was free*/
+    return coalesce(bp);
+}
 
 
 
